83dadfa8238437aa39facdbb07547f4beffd7c36
KevinComba <kelchospense88@gmail.com>
2025-04-02 23:47:32 +0300

    enhance Blog entity with additional properties and comments interface

--- src/blogs/entities/blog.entity.ts
+++ src/blogs/entities/blog.entity.ts
@@ -1,1 +1,19 @@
-export class Blog {}
+interface TComments {
+    id: string;
+    authorName: string;
+    content: string;
+    createdAt: Date;
+}
+
+export class Blog {
+    id: string;
+    title: string;
+    content: string;
+    authorId: string;
+    comments?: string[];
+    tags?: TComments[];
+    isPublished?: boolean;
+    publishedAt?: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}
